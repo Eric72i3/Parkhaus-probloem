194e48beff1d80d303dcb6803657cbc46752a941
Sara76827 <falivenasara@gmail.com>
2026-02-23 09:57:52 +0000

    test

---
+++ Plätze tracking/plätze.java
@@ -0,0 +1,12 @@
+public class Parkhaus{
+    public static void main (String args[]){
+        int gesamt = 200;
+        boolean darfEinfahren;
+
+        if (gesamt>0) {
+            
+        }
+
+
+    }
+}
