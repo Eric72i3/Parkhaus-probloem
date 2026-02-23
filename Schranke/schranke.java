68956af3c1e3c679e2e4943ef2bd1d0f00884f1d
Perlentaucher09 <wespjoshua@gmail.com>
2026-02-23 21:54:49 +0100

    Abfrage wird geändert

--- Schranke/schranke.java
+++ Schranke/schranke.java
@@ -12,28 +12,28 @@ public class Schranke{
     // Die Namen von den Variablen müssen wir noch mit den anderen abgleichen und ich hab die Class jetzt anders genannt eil es sonzt mit den anderen Datein nicht funktziopniert hat
     // Habe auch noch eine while schleife eingebaut damit eine ungültige eingabe nicht das ganze Programm zum absturz bringt
     // Die eingabe ist jetzt auch ein String damit man bei irgendeiner eingabe keinen Absturtz bekommt
-    
+
     public static void main (String args[]){
         boolean invalide_eingabe = true;
         while (invalide_eingabe == true) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Möchten Sie einfahren (1) oder ausfahren (0)?");
+        System.out.println("Möchten Sie einfahren oder ausfahren?");
         String einfahren = sc.nextLine();
 
-        if (einfahren.equals("1") && bezahlt == 1 && plfrei == 1) {
+        if (einfahren.equalsIgnoreCase("einfahren") && bezahlt == 1 && plfrei == 1) {
             invalide_eingabe = false;
             belegt = belegt + 1;
             bezahlt = 0;
             plfrei = 0;// Wird oben neu auf 1 gesetzt
             System.out.println("Einfahrschranke wird geöffnet");
-        } else if (einfahren.equals("0") && bezahlt == 0) {
+        } else if (einfahren.equalsIgnoreCase("einfahren") && bezahlt == 0) {
             invalide_eingabe = false;
             System.out.println("Bitte bezahlen Sie zuerst");
         } 
         // Sollte nie passieren, da man ohne freie Plätze nicht bezahlen kann, aber zur Sicherheit
-        else if (einfahren.equals("1") && plfrei == 0) {
+        else if (einfahren.equalsIgnoreCase("einfahren") && plfrei == 0) {
             System.out.println("Es sind keine freien Plätze mehr vorhanden");
-        }else if (einfahren.equals("0")) {
+        }else if (einfahren.equalsIgnoreCase("ausfahren")) {
             belegt = belegt - 1;
             invalide_eingabe = false;
             System.out.println("Ausfahrschranke wird geöffnet");
