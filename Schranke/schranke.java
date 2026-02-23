5295567af47923bd2dea03b958dfb05bcb38d4ad
Perlentaucher09 <wespjoshua@gmail.com>
2026-02-23 22:26:07 +0100

    while schleife hinzugefügt, die das Programm immer wieder neu starten lässt

--- Schranke/schranke.java
+++ Schranke/schranke.java
@@ -17,42 +17,45 @@ public class Schranke{
     // hat aber nicht funktioniert.
 
     public static void main (String args[]){
-        boolean invalide_eingabe = true;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Möchten Sie einfahren oder ausfahren?");
-        String einfahren = sc.nextLine().trim();
+        while (true) {
+            boolean invalide_eingabe = true;
 
+            System.out.println("Möchten Sie einfahren oder ausfahren?");
+            String einfahren = sc.nextLine().trim();
 
-    if (einfahren.equalsIgnoreCase("einfahren")) {
-    // Der Code der anderen müsste dann hier dazwischen, also sowohl das Prüfen nach freien Plätzen als auch das Bezahlen.
-        } 
-        while (invalide_eingabe == true) {
-        if (einfahren.equalsIgnoreCase("einfahren") && bezahlt == 1 && plfrei == 1) {
-            invalide_eingabe = false;
-            belegt = belegt + 1;
-            bezahlt = 0;
-            plfrei = 0; // Wird oben wieder auf 1 gesetzt
-            System.out.println("Einfahrschranke wird geöffnet");
-        } else if (einfahren.equalsIgnoreCase("einfahren") && bezahlt == 0) {
-            invalide_eingabe = false;
-            System.out.println("Bitte bezahlen Sie zuerst");
-        } 
-        // Sollte nie passieren, da man ohne freie Plätze nicht bezahlen kann, aber zur Sicherheit
-        else if (einfahren.equalsIgnoreCase("einfahren") && plfrei == 0) {
-            System.out.println("Es sind keine freien Plätze mehr vorhanden");
-        }else if (einfahren.equalsIgnoreCase("ausfahren")) {
-            belegt = belegt - 1;
-            invalide_eingabe = false;
-            System.out.println("Ausfahrschranke wird geöffnet");
-        } else {
-            
-            System.out.println("ungültige eingabe"); 
-            System.out.println("Möchten Sie einfahren oder ausfahren? Bitte geben Sie 'einfahren' oder 'ausfahren' ein");
-            einfahren = sc.nextLine().trim();
+            if (einfahren.equalsIgnoreCase("einfahren")) {
+                // Der Code der anderen müsste dann hier dazwischen, also sowohl das Prüfen nach freien Plätzen als auch das Bezahlen.
+            }
+
+            while (invalide_eingabe == true) {
+                if (einfahren.equalsIgnoreCase("einfahren") && bezahlt == 1 && plfrei == 1) {
+                    invalide_eingabe = false;
+                    belegt = belegt + 1;
+                    bezahlt = 0;
+                    plfrei = 0; // Wird oben wieder auf 1 gesetzt
+                    System.out.println("Einfahrschranke wird geöffnet");
+                } else if (einfahren.equalsIgnoreCase("einfahren") && bezahlt == 0) {
+                    invalide_eingabe = false;
+                    System.out.println("Bitte bezahlen Sie zuerst");
+                }
+                // Sollte nie passieren, da man ohne freie Plätze nicht bezahlen kann, aber zur Sicherheit
+                else if (einfahren.equalsIgnoreCase("einfahren") && plfrei == 0) {
+                    System.out.println("Es sind keine freien Plätze mehr vorhanden");
+                } else if (einfahren.equalsIgnoreCase("ausfahren")) {
+                    belegt = belegt - 1;
+                    invalide_eingabe = false;
+                    System.out.println("Ausfahrschranke wird geöffnet");
+                } else {
+                    System.out.println("ungültige eingabe");
+                    System.out.println("Möchten Sie einfahren oder ausfahren? Bitte geben Sie 'einfahren' oder 'ausfahren' ein");
+                    einfahren = sc.nextLine().trim();
+                }
+            }
+
+            bezahlt = 1;
+            plfrei = 1;
+            System.out.println("belegt: " + belegt);
         }
-    }
-    bezahlt = 1;
-    plfrei = 1;
-    System.out.println("belegt: " + belegt);
 }
 }
