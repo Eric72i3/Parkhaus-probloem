5b8d29473c0cd51abed2b13cd5b30916620c9187
Sara76827 <falivenasara@gmail.com>
2026-02-26 12:39:16 +0100

    Update plätze.java

--- Plätze tracking/plätze.java
+++ Plätze tracking/plätze.java
@@ -1,6 +1,15 @@
 public class Parkhaus{
+    public static int frei(int gesamt, int besetzt) {return gesamt-besetzt;}
+    
     public static void main (String args[]){
-        int einfahren;
+    
+    int gesamt=200;
+    int besetzt=0;
+    int freieplaetze = gesamt-besetzt;
+
+    System.out.println(freieplaetze);
+
+    int einfahren;
 
         if (freieplaetze>0) {
             einfahren = 1;
@@ -9,5 +18,6 @@ public class Parkhaus{
             einfahren = 0;
         }
         System.out.println("Darf einfahren: " + einfahren);
-    }
+
+  }
 }
